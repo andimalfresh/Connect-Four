c0ccfc30f4cedf2af919e0825e8ff9f3994fb321
daneparke <dapa8522@colorado.edu>
2018-10-12 10:30:58 -0600

    basic react framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Board from './components/Board';
 
 class App extends Component {
+  state = {
+    turnCount = 0,
+    winLength = 4,
+  }
+
+
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div class='container bodyCont'>
+          <div class='container mainCont'>
+            <Header />
+            <Board />
+          </div>
+        </div>
+
+      </>
+
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+
+const Header = () => {
+    return (
+
+        <nav className="row">
+            <h1>Connect Four!</h1>
+        </nav>
+    )
+}
+
+export default Header
